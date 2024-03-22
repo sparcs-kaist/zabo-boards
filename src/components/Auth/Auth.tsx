25fa5a1e581acfe931c64b6e3d0590533d3c02e7
Jinho-Choi123 <choijinho817@gmail.com>
2024-03-23 00:36:48 +0900

    refactor: separate diff pages to diff file

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch, useAppSelector } from "@/types";
 import { type RootState } from "@/redux/store";
 import { Board } from "@/components/Board";
 import { loginThunk } from "@/redux/auth/loginThunk";
-import style from "./Auth.module.scss";
+import { LoginPage } from "@/components/Auth/LoginPage";
 
 export const AuthPage = () => {
   const [deviceId, setDeviceId] = useState("");
@@ -33,50 +33,14 @@ export const AuthPage = () => {
     setPin("");
   };
 
-  if (isLoggedIn) {
-    return <Board />;
-  }
-
-  return (
-    <div className={style.modalWrapper}>
-      <div className={style.modalContainer}>
-        <h1 className={style.modalTitle}> Zabo Board Login</h1>
-        <br />
-        <p className={style.modalContent}>
-          Zabo Board 서비스를 이용하기 위해서는 등록된 device Id와 PIN을
-          입력하세요.
-          <br />
-          Zabo Board 서비스를 등록하기 위해서는 zabo.sparcs.org 채널톡으로
-          문의해주세요.
-        </p>
-        <div className={style.form}>
-          <label className={style.input}>
-            device Id
-            <input
-              className={style.input}
-              name="deviceId"
-              onChange={onDeviceIdChange}
-            />
-          </label>
-          <label className={style.input}>
-            PIN
-            <input
-              className={style.input}
-              type="password"
-              name="PIN"
-              onChange={onPinChange}
-            />
-          </label>
-        </div>
-        <button
-          type="submit"
-          className={style.submit}
-          onClick={onSubmitHandler}
-        >
-          Submit
-        </button>
-        <div className={style.errorBox}>{errorMessage}</div>
-      </div>
-    </div>
+  return isLoggedIn ? (
+    <Board />
+  ) : (
+    <LoginPage
+      errorMessage={errorMessage}
+      onDeviceIdChange={onDeviceIdChange}
+      onPinChange={onPinChange}
+      onSubmitHandler={onSubmitHandler}
+    />
   );
 };
